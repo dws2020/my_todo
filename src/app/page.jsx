3475dbce0408bf209029642f14bca4faf8c19d18
dws2020 <73006643+dws2020@users.noreply.github.com>
2023-05-16 21:28:58 +0900

    page.jsx rename

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Input from "./components/Input";
+import Todo from "./components/Todo";
+
+export default function Home() {
+  return (
+    <>
+      <Input />
+      <Todo />
+    </>
+  );
+}
